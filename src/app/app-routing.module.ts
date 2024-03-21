c338fc9ef1c8d8ca3d32749efed05efedf0d3464
giuliacimino <giuliacimino00@gmail.com>
2024-03-21 16:26:17 +0100

    commit per silvano: completata homepage e listabadge. Da completare inserimento badge.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./src/app/features/homepage/homepage.component";
+import {BadgeListComponent} from "./src/app/features/badge/badge-list/badge-list.component";
+import {BadgeInsertComponent} from "./src/app/features/badge/badge-insert/badge-insert.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'badge/insert', component: BadgeInsertComponent },
+  { path: 'badge/list', component: BadgeListComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './src/app/core/navbar/navbar.component';
+import { FooterComponent } from './src/app/core/footer/footer.component';
+import { HomepageComponent } from './src/app/features/homepage/homepage.component';
+import { BadgeListComponent } from './src/app/features/badge/badge-list/badge-list.component';
+import { BadgeInsertComponent } from './src/app/features/badge/badge-insert/badge-insert.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    BadgeListComponent,
+    BadgeInsertComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
